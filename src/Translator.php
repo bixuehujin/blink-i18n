96708c069455834a8386261afb06bad35df35216
Jin Hu <bixuehujin@gmail.com>
2016-01-04 18:05:12 +0800

    Add files

---
+++ src/Translator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace blink\i18n;
+
+use Symfony\Component\Translation\Translator as BaseTranslator;
+
+
+/**
+ * Class Translator
+ *
+ * @package blink\i18n
+ */
+class Translator extends BaseTranslator
+{
+
+}
