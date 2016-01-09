ce4300c0be526b5760dde76b3989229b4ca3c777
Jin Hu <bixuehujin@gmail.com>
2016-01-09 12:52:31 +0800

    Basic functionality

--- src/Translator.php
+++ src/Translator.php
@@ -2,6 +2,9 @@
 
 namespace blink\i18n;
 
+use blink\core\Configurable;
+use blink\core\ObjectTrait;
+use blink\core\InvalidParamException;
 use Symfony\Component\Translation\Translator as BaseTranslator;
 
 
@@ -10,7 +13,39 @@ use Symfony\Component\Translation\Translator as BaseTranslator;
  *
  * @package blink\i18n
  */
-class Translator extends BaseTranslator
+class Translator extends BaseTranslator implements Configurable
 {
+    use ObjectTrait;
 
+    public $debug = false;
+    public $cacheDir;
+    public $defaultLocale = 'en-US';
+    public $loaders = [];
+    public $resources = [];
+
+    public function __construct($config = [])
+    {
+        foreach ($config as $name => $value) {
+            $this->$name = $value;
+        }
+
+        parent::__construct($this->defaultLocale, null, $this->cacheDir, $this->debug);
+
+        $this->init();
+    }
+
+    public function init()
+    {
+        foreach ($this->loaders as $format => $loader) {
+            $loader = make($loader);
+            $this->addLoader($format, $loader);
+        }
+
+        foreach ($this->resources as $resource) {
+            if (!isset($resource['format'], $resource['resource'], $resource['locale'])) {
+                throw new InvalidParamException('The resource item requires format, resource and locale keys.');
+            }
+            $this->addResource($resource['format'], $resource['resource'], $resource['locale'], isset($resource['domain']) ? $resource['domain'] : null);
+        }
+    }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * Returns the i18n component.
+ *
+ * @return \blink\i18n\Translator
+ */
+function i18n()
+{
+    return app('i18n');
+}
+
+/**
+ * Translates the given message.
+ *
+ * @param $id
+ * @param array $parameters
+ * @param null $domain
+ * @param null $locale
+ * @return string The translated string
+ */
+function translate($id, $parameters = [], $domain = null, $locale = null)
+{
+    return i18n()->trans($id, $parameters, $domain, $locale);
+}
